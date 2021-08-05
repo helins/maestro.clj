8ad11325404fb30167084b482c4ffd50c6c1ef07
Adam Helinski <adam@helins.io>
2021-08-05 19:31:46 +0200

    Add target selection
    Instead of a mere keyword, an alias can be represented as a map of
    `symbol` -> `keyword`.
    
    First CLI argument can be a symbol which will effectively act as a
    selector. This is useful for managing internal dependencies. Sometimes,
    an internal dependency is needed as local source, sometimes as a
    released jar.

--- project/core/src/main/helins/maestro.clj
+++ project/core/src/main/helins/maestro.clj
@@ -56,21 +56,27 @@
 
   ([ctx alias+]
 
-   (let [user-aggr (ctx :maestro/aggr)]
+   (let [user-aggr (ctx :maestro/aggr)
+         target    (ctx :maestro/target)]
      (reduce (fn [ctx-2 alias]
-               (if (contains? (ctx-2 :maestro/seen+)
-                              alias)
-                 ctx-2
-                 (let [data ($.alias/data ctx
-                                          alias)]
-                   (user-aggr (walk (update ctx-2
-                                            :maestro/seen+
-                                            (fnil conj
-                                                  #{})
-                                            alias)
-                                     (:maestro/require data))
-                               alias
-                               data))))
+               (let [alias-2 (if (map? alias)
+                               (get alias
+                                    target)
+                               alias)]
+                 (if (or (nil? alias-2)
+                         (contains? (ctx-2 :maestro/seen+)
+                                    alias-2))
+                   ctx-2
+                   (let [data ($.alias/data ctx
+                                            alias-2)]
+                     (user-aggr (walk (update ctx-2
+                                              :maestro/seen+
+                                              (fnil conj
+                                                    #{})
+                                              alias-2)
+                                       (:maestro/require data))
+                                 alias-2
+                                 data)))))
              (update ctx
                      :maestro/seen+
                      (fn [seen+]
@@ -101,6 +107,56 @@
 ;;;;;;;;;; Initialization
 
 
+(defn- -parse-alias+
+
+  ;;
+
+  [string]
+
+  (or (and (clojure.string/starts-with? string
+                                        ":")
+           (->> (clojure.string/split string
+                                      #":")
+                rest
+                (mapv keyword)
+                not-empty))
+      (throw (ex-info "At least one alias must be specified"
+                      {:maestro/arg string}))))
+
+
+
+(defn- -parse-begin
+
+  ;;
+
+  [^String string]
+
+  (cond
+    (clojure.string/starts-with? string
+                                 "-")
+    (try
+      [(-parse-alias+ (.substring string
+                                  2))
+       (nth string
+            1)]
+      (catch Throwable _ex
+        (throw (ex-info "First argument starting with '-' needs a Clojure execution letter (eg. '-M')"
+                        {:maestro/arg string}))))
+
+
+    (clojure.string/starts-with? string
+                                 ":")
+    [(-parse-alias+ string)
+     nil]
+
+
+    :else
+    nil))
+
+
+
+
+
 (defn ctx
 
   "A context designates the map that is omnipresent throughout all this tool.
@@ -125,35 +181,34 @@
    (let [arg-first    (or (first cli-arg+)
                           (throw (ex-info "No argument provided, requires at least one alias"
                                           {})))
-         [arg-first-2
-          exec-char]  (if (clojure.string/starts-with? arg-first
-                                                       "-")
-                        (try
-                          [(.substring ^String arg-first
-                                       2)
-                           (nth arg-first
-                                1)]
-                          (catch Throwable _ex
-                            (throw (ex-info "First argument starting with '-' needs a Clojure execution letter (eg. '-M')"
-                                            {:maestro/arg arg-first}))))
-                        [arg-first
-                         nil])]
+
+         [alias+
+          cli-arg-2+
+          exec-char
+          target]     (if-some [[alias+
+                                 exec-char] (-parse-begin arg-first)]
+                        [alias+
+                         (rest cli-arg+)
+                         exec-char
+                         'default]
+                        (if-some [[alias+
+                                   exec-char] (-parse-begin (second cli-arg+))]
+                          [alias+
+                           (drop 2
+                                 cli-arg+)
+                           exec-char
+                           (symbol arg-first)]
+                          (throw (ex-info "Must provide aliases"
+                                          {:maestro/arg (second cli-arg+)}))))]
       (-> deps-edn
           (merge (if (.ready *in*)
                    (clojure.edn/read *in*)
                    {}))
           (assoc :maestro/aggr    aggr
-                 :maestro/arg+    (rest cli-arg+)
-                 :maestro/main+   (or (and (clojure.string/starts-with? arg-first-2
-                                                                        ":")
-                                           (->> (clojure.string/split arg-first-2
-                                                                      #":")
-                                                rest
-                                                (mapv keyword)
-                                                not-empty))
-                                      (throw (ex-info "At least one alias must be specified"
-                                                      {:maestro/arg arg-first-2})))
-                 :maestro/require [])
+                 :maestro/arg+    cli-arg-2+
+                 :maestro/main+   alias+
+                 :maestro/require []
+                 :maestro/target  target)
           (cond->
             exec-char
             (assoc :maestro/exec-char exec-char))))))
